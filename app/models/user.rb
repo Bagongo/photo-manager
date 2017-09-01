b8f744a587610320625d3b12cb0ce447ec52dac4
Ale <akak@hotmail.it>
2017-09-01 19:20:18 +0200

    Implemented image scaffold, image upload with Carrierwave and image validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :payment
-  accepts_nested_attributes_for :payment          
+  accepts_nested_attributes_for :payment  
+  has_many :images        
 
 end
